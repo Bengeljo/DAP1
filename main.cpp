1b5cd19de77a76b67004df46ce41b3ef6cfdef42
Bengeljo <86926513+Bengeljo@users.noreply.github.com>
2024-04-11 07:45:45 +0000

    11.04

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <cstdio> // stellt das Kommando printf zur Verfügung
+
+int main(){
+    int n; //int bedeutet, dass n eine ganze Zahl ist (Integer)
+    int nquadrat; // nquadrat ist das Quadrat von n
+    n = 42; //n wird auf 42 gesetzt
+    nquadrat = n * n; //nquadrat wird berechnet
+    printf("Das Quadrat von %i ist %i", n, nquadrat); //Output auf der Konsole
+}
